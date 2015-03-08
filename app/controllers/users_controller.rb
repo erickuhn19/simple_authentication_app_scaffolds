e634874391f9f8be6fc575f018ccbc6ccaddfccc
Eric Kuhn <erickuhn19@gmail.com>
2015-03-08 13:59:09 -0400

    Create articles scaffold, add header that links to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id 
+			redirect_to articles_index_path
+		else
+			redirect_to new_user_path
+		end
+
+	end
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
 	end
 
 
